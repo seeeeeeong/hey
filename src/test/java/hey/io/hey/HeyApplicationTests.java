e7b2f9b91796a65bc52c0c62ed318e35b52381ce
seeeeeeong <lsinsung@gmail.com>
2024-06-25 05:41:50 +0900

    first commit

---
+++ src/test/java/hey/io/hey/HeyApplicationTests.java
@@ -0,0 +1,13 @@
+package hey.io.hey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
