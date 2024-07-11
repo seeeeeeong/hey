aa19c7e7d1803390218220441f273c297027605a
seeeeeeong <lsinsung@gmail.com>
2024-07-11 17:35:20 +0900

    feat: follow test

--- src/test/java/hey/io/hey/domain/follow/controller/PerformanceFollowControllerTest.java
+++ src/test/java/hey/io/hey/domain/follow/controller/PerformanceFollowControllerTest.java
@@ -3,6 +3,7 @@ package hey.io.hey.domain.follow.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hey.io.hey.common.response.SliceResponse;
 import hey.io.hey.common.security.jwt.JwtTokenProvider;
+import hey.io.hey.domain.follow.dto.DeleteFollowRequest;
 import hey.io.hey.domain.follow.dto.FollowResponse;
 import hey.io.hey.domain.follow.service.FollowService;
 import hey.io.hey.domain.performance.dto.PerformanceResponse;
@@ -23,10 +24,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,9 +58,27 @@ public class PerformanceFollowControllerTest {
     public void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(new FollowController(followService)).build();
     }
+
+    private DeleteFollowRequest createDeleteFollowRequest() throws Exception {
+        Constructor<DeleteFollowRequest> constructor = DeleteFollowRequest.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        DeleteFollowRequest request = constructor.newInstance();
+
+        List<String> ids = Collections.singletonList("1");
+
+        setField(request, "ids", ids);
+        return request;
+    }
+
+    private void setField(Object object, String fieldName, Object value) throws Exception {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(object, value);
+    }
+
     @Test
     @WithMockUser
-    void follow() throws Exception {
+    void followPerformance() throws Exception {
         // given
         Long userId = 1L;
         String accessToken = "Bearer " + jwtTokenProvider.createAccessToken(userId, UserRole.USER);
@@ -64,7 +89,7 @@ public class PerformanceFollowControllerTest {
 
         // when
         when(followService.followPerformance(userId, performanceId)).thenReturn(followResponse);
-        ResultActions result = mockMvc.perform(post("/users/follow/{id}", performanceId)
+        ResultActions result = mockMvc.perform(post("/users/follow/performances/{id}", performanceId)
                 .with(SecurityMockMvcRequestPostProcessors.csrf())
                 .header("Authorization", accessToken)
                 .contentType(MediaType.APPLICATION_JSON));
@@ -75,7 +100,7 @@ public class PerformanceFollowControllerTest {
 
     @Test
     @WithMockUser
-    void getFollow() throws Exception {
+    void getFollowPerformances() throws Exception {
         // given
         Long userId = 1L;
         String accessToken = "Bearer " + jwtTokenProvider.createAccessToken(userId, UserRole.USER);
@@ -86,7 +111,7 @@ public class PerformanceFollowControllerTest {
 
         // when
         when(followService.getFollowPerformances(any(), eq(20), eq(0), eq(Sort.Direction.DESC))).thenReturn(sliceResponse);
-        ResultActions result = mockMvc.perform(get("/users/follow")
+        ResultActions result = mockMvc.perform(get("/users/follow/performances")
                 .with(SecurityMockMvcRequestPostProcessors.csrf())
                 .header("Authorization", accessToken)
                 .contentType(MediaType.APPLICATION_JSON));
@@ -97,23 +122,87 @@ public class PerformanceFollowControllerTest {
 
     @Test
     @WithMockUser
-    void deleteFollow() throws Exception {
+    void deleteFollowPerformances() throws Exception {
         // given
         Long userId = 1L;
         String accessToken = "Bearer " + jwtTokenProvider.createAccessToken(userId, UserRole.USER);
 
-        String performanceId = "performanceId";
-
-        FollowResponse followResponse = new FollowResponse(performanceId, "UnFollow Success");
+        DeleteFollowRequest deleteFollowRequest = createDeleteFollowRequest();
 
         // when
-        when(followService.followPerformance(userId, performanceId)).thenReturn(followResponse);
-        ResultActions result = mockMvc.perform(delete("/users/follow/{id}", performanceId)
+        doNothing().when(followService).deleteFollowPerformances(eq(userId), eq(deleteFollowRequest.getIds()));
+        ResultActions result = mockMvc.perform(delete("/users/follow/performances")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .header("Authorization", accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(deleteFollowRequest)));
+
+        // then
+        result.andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithMockUser
+    void followArtist() throws Exception {
+        // given
+        Long userId = 1L;
+        String accessToken = "Bearer " + jwtTokenProvider.createAccessToken(userId, UserRole.USER);
+
+        String artistId = "artistId";
+
+        FollowResponse followResponse = new FollowResponse(artistId, "Follow Success");
+
+        // when
+        when(followService.followPerformance(userId, artistId)).thenReturn(followResponse);
+        ResultActions result = mockMvc.perform(post("/users/follow/artists/{id}", artistId)
                 .with(SecurityMockMvcRequestPostProcessors.csrf())
                 .header("Authorization", accessToken)
                 .contentType(MediaType.APPLICATION_JSON));
 
         // then
         result.andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    void getFollowArtists() throws Exception {
+        // given
+        Long userId = 1L;
+        String accessToken = "Bearer " + jwtTokenProvider.createAccessToken(userId, UserRole.USER);
+
+        String artistId = "artistId";
+
+        SliceResponse<PerformanceResponse> sliceResponse = new SliceResponse<>(new SliceImpl<>(Collections.emptyList(), PageRequest.of(0, 20), false));
+
+        // when
+        when(followService.getFollowPerformances(any(), eq(20), eq(0), eq(Sort.Direction.DESC))).thenReturn(sliceResponse);
+        ResultActions result = mockMvc.perform(get("/users/follow/artists")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .header("Authorization", accessToken)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    void deleteFollowArtists() throws Exception {
+        // given
+        Long userId = 1L;
+        String accessToken = "Bearer " + jwtTokenProvider.createAccessToken(userId, UserRole.USER);
+
+        DeleteFollowRequest deleteFollowRequest = createDeleteFollowRequest();
+
+        // when
+        doNothing().when(followService).deleteFollowPerformances(eq(userId), eq(deleteFollowRequest.getIds()));
+        ResultActions result = mockMvc.perform(delete("/users/follow/artists")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .header("Authorization", accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(deleteFollowRequest)));
+
+        // then
+        result.andExpect(status().is2xxSuccessful());
     }
 }
